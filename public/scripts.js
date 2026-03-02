95914f4f0db3f44c5d50562be32ce694bcbd9c0b
awakesid <youcrashall@gmail.com>
2026-03-02 10:45:03 +0545

    added blog optimizing scripts

--- public/scripts.js
+++ public/scripts.js
@@ -4,31 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-const btn = document.getElementById("mailBtn");
-  btn.addEventListener("click", () => {
-    const email = "youcrashall@gmail.com";
-    navigator.clipboard.writeText(email).then(() => {
-      const tooltip = btn.querySelector(".tooltip-text");
-      // tooltip.classList.remove("tooltip-text"); // reset animation
-      tooltip.classList.add("show");
-      setTimeout(() => tooltip.classList.remove("show"),1500);
-    }).catch(err => console.error("Failed to copy email: ", err));
-  });
+  const btn = document.getElementById("mailBtn");
+  if (btn) {
+    btn.addEventListener("click", () => {
+      const email = "youcrashall@gmail.com";
+      navigator.clipboard.writeText(email).then(() => {
+        const tooltip = btn.querySelector(".tooltip-text");
+        // tooltip.classList.remove("tooltip-text"); // reset animation
+        tooltip.classList.add("show");
+        setTimeout(() => tooltip.classList.remove("show"), 1500);
+      }).catch(err => console.error("Failed to copy email: ", err));
+    });
+  }
 
-  toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    // document.imagemode.classList.toggle("imagedarkmode");
+  if (toggleBtn) {
+    toggleBtn.addEventListener("click", () => {
+      document.body.classList.toggle("dark");
+      // document.imagemode.classList.toggle("imagedarkmode");
 
-        if (document.body.classList.contains("dark")) {
-      localStorage.setItem("theme", "dark");
-    } else {
-      localStorage.setItem("theme", "light");
-    }
+      if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+      } else {
+        localStorage.setItem("theme", "light");
+      }
+    });
+  }
 
-
-  });
-
-    if (localStorage.getItem("theme") === "dark") {
+  if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark");
   }
 
